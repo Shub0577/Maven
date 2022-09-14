2415e970e34db4ac1b864fe7ed81a6662a35a69f
Shub07 <shubhamalok111@gmail.com>
2022-09-14 21:55:19 +0530

    first Commit

---
+++ src/main/java/com/example/demo/AssignmentYahooConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentYahooConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentYahooConfigServerApplication.class, args);
+		System.out.println("Config server is up and running @8779.....");
+	}
+
+}
